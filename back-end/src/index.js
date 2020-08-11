82cd8dc2d53307674603a65afd0b870529fc55d3
Tuomas Röpelinen <tuomas.m.r@gmail.com>
2020-08-11 22:34:35 +0300

    Form datan syöttö tietokantaan korjattu

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const connectDB = require("./db.js");
+const sql = require("mssql/msnodesqlv8");
+const config = require("./dbConfig.js");
+const shortid = require("shortid");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
 app.post("/shorten", (req, res) => {
-    
+    sql.connect(config, (err) => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log("Tietokanta yhteys avattu.");
+        }
+
+        let request = new sql.Request();
+        let sqlQuery = "SELECT * FROM Url WHERE OriginalUrl='" + req.body.originalUrl + "'";
+        request.query(sqlQuery, (err, data) => {
+            if(err) {
+                console.log(err);
+                sql.close();
+            }
+            console.log(JSON.stringify(data.recordset));
+            if(JSON.stringify(data.recordset) === "[]"){
+                let id = shortid.generate();
+                sqlQuery = "INSERT INTO Url (OriginalURL, ShortID, ShortURL) VALUES ('" + req.body.originalUrl + "', '" + id + "', 'http://localhost:3000/" + id + "')";
+                request.query(sqlQuery, (err, data) => {
+                    if(err){
+                        console.log(err);
+                        sql.close();
+                    }
+                });
+            }
+        });
+    });
 });
 
 app.listen(5000, () => {
--- front-end/src/UrlForm.js
+++ front-end/src/UrlForm.js
@@ -2,11 +2,32 @@ import React from "react";
 import "./UrlForm.css";
 
 class UrlForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+             originalUrl: "",
+             urlToPost: ""
+            }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const response = await fetch("/shorten", {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ originalUrl: this.state.originalUrl })
+        });
+        const body = await response.text();
+        this.setState({ urlToPost: body })
+    };
+
     render() {
         return(
             <div>
-                <form action="/shorten" method="post" name="urlForm">
-                    <input type="text" name="long_url" placeholder="Enter your URL"></input>
+                <form onSubmit={this.handleSubmit} action="/shorten" method="post" name="urlForm">
+                    <input type="text" value={this.state.originalUrl} onChange={event => this.setState({ originalUrl: event.target.value })} name="long_url" placeholder="Enter your URL"></input>
                     <button type="submit">Shorten</button>
                 </form>
             </div>
